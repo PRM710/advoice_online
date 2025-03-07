dc1425a734097a8caf5ab1646c3640bb8ad4934b
PRM710 <99472825+PRM710@users.noreply.github.com>
2025-03-07 20:40:51 +0530

    Update recog.js

--- scripts/recog.js
+++ scripts/recog.js
@@ -177,6 +177,7 @@ export function handleRecognition() {
       .replaceAll("ॲट द रेट", "@")
       .replaceAll("एक्स्प्लानेशन मार्क", "!")
       .replaceAll("एक्सक्लामेषण मार्क", "!")
+      .replaceAll("एक्सप्लेनेशन मार्क", "!")
       .replaceAll("क्वेश्चन मार्क", "?")
       .replaceAll("अँपरसँड", "&")
       .replaceAll("न्यू लाईन", "<br/>")
